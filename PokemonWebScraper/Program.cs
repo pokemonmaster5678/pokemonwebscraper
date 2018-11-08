0f512abd60218544fbb7ae345be9a2dc50bf88b7
Albert.Kim <albert1usainc@gmail.com>
2018-11-08 10:07:17 -0500

    new pokemonwebscraper project

---
+++ PokemonWebScraper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace PokemonWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://pokemondb.net/pokedex/national
+            var client = new HttpClient();
+
+            var result = client.GetStringAsync("https://pokemondb.net/pokedex/national").Result;
+        }
+    }
+}
